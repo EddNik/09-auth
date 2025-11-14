51adecc1fcab84a0575c52b4fcbcba3717286dfc
Eduard <nikinna86@gmail.com>
2025-11-14 21:06:23 +0200

    update

---
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -0,0 +1,67 @@
+"use client";
+import { useParams } from "next/navigation";
+import css from "./NotePreview.module.css";
+import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { Note } from "@/types/notes";
+import Loader from "@/app/loading";
+import Error from "@/app/error";
+import Modal from "@/components/Modal/Modal";
+import { fetchNoteById } from "@/lib/api/clientApi";
+
+function NotePreviewClient() {
+  const { id } = useParams<{ id: string }>();
+
+  const router = useRouter();
+
+  const {
+    data: note,
+    isLoading,
+    isError,
+    error,
+  } = useQuery<Note>({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <Loader />;
+
+  if (isError) return <Error error={error} />;
+
+  const onClose = () => {
+    router.back();
+  };
+
+  const formatDate = (isoDate: string) => {
+    return new Date(isoDate).toLocaleString("en-UA", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
+
+  return (
+    <Modal onClose={onClose}>
+      <div className={css.container}>
+        {note && (
+          <div className={css.item}>
+            <div className={css.header}>
+              <h2>{note.title}</h2>
+            </div>
+            <p className={css.content}>{note.content}</p>
+            <p className={css.tag}>{note.tag}</p>
+            <p className={css.date}>{formatDate(note.createdAt)}</p>
+          </div>
+        )}
+        <button onClick={onClose} type="button" className={css.cancelButton}>
+          Back
+        </button>
+      </div>
+    </Modal>
+  );
+}
+
+export default NotePreviewClient;
---
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -0,0 +1,26 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import NotePreviewClient from "./NotePreview.client";
+import { fetchNoteById } from "@/lib/api/serverApi";
+
+async function NotePreview({ params }: { params: Promise<{ id: string }> }) {
+  const id = (await params).id;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotePreviewClient />
+    </HydrationBoundary>
+  );
+}
+
+export default NotePreview;
